9857f8b6eac3168f906faa78029a27ae1d0d8061
youngrrrr <young-rae_kim@brown.edu>
2016-03-20 03:18:09 -0400

    removed resume

--- assets/js/google-button.js
+++ assets/js/google-button.js
@@ -1,6 +1,6 @@
 const numLi = 4;
 
-$("button").hover(function(){
+$("button").hover(function() {
 	var self = this;
 	var pos = -(Math.floor(Math.random() * (numLi - 1)) + 1) * 55
 	setTimeout(function(){
@@ -8,4 +8,10 @@ $("button").hover(function(){
 	}, 500);
 }, function(){
 	$(this).find("ul").css("margin-top", "0px");
-})
+});
+
+$("button").dblclick(function() {
+	console.log("a");
+	$(this).remove("button > ul");
+	$(this).append("<a href=\"mailto:young-rae_kim@brown.edu\" target=\"_blank\">Let's Get Acquainted</a><br><a href=\"https://www.linkedin.com/in/youngraekim\" target=\"_blank\">Let's Get Professional</a><br><a href=\"https://www.facebook.com/youngrae.95\" target=\"_blank\">Let's Get Friendly</a><br><a href=\"https://github.com/youngrrrr\" target=\"_blank\">Let's Get To Work</a>");
+});
